773aaa5337c13c8760f32c1db919db6e2b1380fe
Paul Putier <paul.putier@epita.fr>
2020-10-10 11:47:19 +0200

    launch: add some error handling

--- launch.c
+++ launch.c
@@ -8,16 +8,31 @@ int launch_mission(struct rocket *rocket)
     printf("Last checks before rocket launch:\n");
     sleep(1);
 
-    int astronaut_count = rocket->astronaut_count;
+    /* Is there a rocket to launch */
+    if (!rocket)
+    {
+        printf("No rocket found. Mission abort.\n");
+        return 0;
+    }
 
+
+    /* Is the rocket full */
+    int astronaut_count = rocket->astronaut_count;
     int rocket_full = astronaut_count == 3;
+
     printf("Checking travel gear: OK\n");
     printf("Checking austronauts count: %d/3 - %s\n",
             astronaut_count,
             rocket_full ? "OK" : "KO"
     );
 
+    if (!rocket_full) {
+        printf("Missing astronauts. Mission abort.\n");
+        return 1;
+    }
 
+
+    /* Is swap_seats working */
     int swap_res = swap_seats(rocket,
                               rocket->astronauts[0]->name,
                               rocket->astronauts[2]->name);
@@ -25,10 +40,12 @@ int launch_mission(struct rocket *rocket)
     printf("Checking seats: %s\n", swap_res ? "OK" : "KO");
 
     if (!rocket_full) {
+        printf("Astronauts cannot swap seats. Mission abort.\n");
         return 1;
     }
 
 
+    /* Launch */
     printf("\nLaunching rocket in ");
     fflush(stdout);
     sleep(1);
